e6c45009ae0f278adf6c4911897b0961838e7fd9
Mihindum.Liyanage <Mihindum.Liyanage@DESKTOP-5NPTAP3>
2023-10-26 16:43:08 +0530

    Delete Method

--- ASPNETCoreAPI/Controllers/VillaController.cs
+++ ASPNETCoreAPI/Controllers/VillaController.cs
@@ -16,19 +16,16 @@ namespace ASPNETCoreAPI.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("{id:int}", Name = "GetaVilla")]
 
         public ActionResult<VillaDTO> GetVillasById(int id)
         {
-            if(id == 0)
+            if (id == 0)
             {
                 return BadRequest();
             }
             var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
-            if(villa == null)
+            if (villa == null)
             {
                 return NotFound();
             }
@@ -36,23 +33,35 @@ namespace ASPNETCoreAPI.Controllers
         }
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
             if (villaDTO == null)
             {
                 return BadRequest();
             }
-            if (villaDTO.Id > 0) 
-            { 
-                return StatusCode(StatusCodes.Status500InternalServerError); 
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
-            villaDTO.Id = VillaStore.villaList.OrderByDescending(u=>u.Id).FirstOrDefault().Id + 1;
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
             VillaStore.villaList.Add(villaDTO);
-            
-            return Ok(villaDTO);    
+
+            return Ok(villaDTO);
+        }
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        public IActionResult DeleteVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return Ok();
         }
     }
 } 
